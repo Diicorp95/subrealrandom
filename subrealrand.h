55a102a09c5ca272190ba32da2eb3f9c11d1510e
Diicorp95 <45205643+Diicorp95@users.noreply.github.com>
2021-02-10 19:29:08 +0300

    Create subrealrand.h

---
+++ subrealrand.h
@@ -0,0 +1,11 @@
+/*
+  Label: Sub Real Random library
+  Version: 0.1a
+  Description:
+  The goal is to get almost real random number by randomizing a prime number with complex algorithms.
+
+  Licensed under GNU GPLv3
+  The copyright belongs to Diicorp95, 2021.
+*/
+
+int subrealrandom(int minimum, maximum);
